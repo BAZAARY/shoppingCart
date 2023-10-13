7584c8ee80b6cb48170f69aece8c62114a6571a3
@Bluruwu <juandiegotres@hotmail.com>
2023-10-13 15:46:06 -0500

    vista terminada

--- shoppingCart/resumenCompra/views.py
+++ shoppingCart/resumenCompra/views.py
@@ -4,12 +4,15 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from resumenCompra.models import Carrito
 from rest_framework.decorators import permission_classes
-
+from resumenCompra.serializers import CarritoSerializer
 # Create your views here.
 
 class ResumenCarrito(APIView):
+    #Retorna todos los elementos del carrito
+    #Este metodo se actualizará eventualmente cuando pueda recuperar el token de auth
     permission_classes = (permissions.AllowAny)
 
     def get(self, request, format=None):
         queryset = Carrito.objects.all()
-        serializer =
+        serializer = CarritoSerializer(queryset, many=True)
+        return Response(serializer.data)
